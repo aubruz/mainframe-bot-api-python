241acd118a44ada10a82e53ad46a1b2f6d764cc8
Aubruz <aubruz@hotmail.com>
2017-11-10 16:33:18 +0100

    Update setup.py

--- Setup.py
+++ Setup.py
@@ -15,4 +15,22 @@ setup(
                      'build forms, dialogs and messages.',
     zip_false=False,
     keywords='mainframe bot api',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
