ecf1005e8329f2ab6aad8d88ad3c8aac3ab413ea
Aubruz <aubruz@hotmail.com>
2017-11-10 15:12:19 +0100

    Add setup.py

---
+++ Setup.py
@@ -0,0 +1,17 @@
+# _*_ coding: utf-8 _*_
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name="mainframe-api",
+    version="1.0.0",
+    author="aubruz",
+    packages=find_packages(),
+    url="https://github.com/aubruz/mainframe-api-python",
+    license="MIT License",
+    description="Communicate easily with Mainframe API",
+    long_description='Mainframe API includes a Client to make calls easily, '
+                     'responses objects and a complete set of UI components to '
+                     'build forms, dialogs and messages.',
+    zip_false=False
+)
